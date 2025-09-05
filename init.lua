3b5a3485f531a640b49e7d2b92396e3fae6a0fb1
Michael R. Taylor <michael@atomic.city>
2025-09-05 18:03:54 -0500

    Simplified init and made it synchronous to help configuration

--- init.lua
+++ init.lua
@@ -18,32 +18,24 @@
 --[[
   TODO:
   * Highlight current completion (maybe also change to TAB to select)
-  * Remove modeline in insert mode
   * LSP diagnostics
 ]]
 
---[[ File-type specific options ]]
-local augroup = vim.api.nvim_create_augroup("michaeltaylor.cfg", { clear = true })
-local autocmd = vim.api.nvim_create_autocmd
+require("core.std")
+require("core.options")
 
-local function load_file(path)
-  local co = coroutine.running()
-  vim.defer_fn(function()
+--Create an autogroup to use during config (set "clear" here, but not elseware)
+local augroup = vim.api.nvim_create_augroup("michaeltaylor.cfg", { clear = true })
+
+local groups = {
+  vim.api.nvim_get_runtime_file("lua/plugins/*.lua", true),
+  vim.api.nvim_get_runtime_file("lua/languages/*.lua", true),
+}
+for _, files in ipairs(groups) do
+  for _, path in ipairs(files) do
     dofile(path)
-    coroutine.resume(co)
-  end, 2)
-  coroutine.yield()
+  end
 end
 
-coroutine.wrap(function()
-  local groups = {
-    vim.api.nvim_get_runtime_file("lua/core/*.lua", true),
-    vim.api.nvim_get_runtime_file("lua/plugins/*.lua", true),
-    vim.api.nvim_get_runtime_file("lua/languages/*.lua", true),
-  }
-  for _, files in ipairs(groups) do
-    for _, path in ipairs(files) do
-      load_file(path)
-    end
-  end
-end)()
+-- Wait until after plugins are loaded to set the keymaps
+require("core.keymaps")
